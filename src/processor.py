257abfa3240981cd15947f2ee0edc66c853624ab
khan <m.khanddorj@gmail.com>
2022-10-13 23:54:27 +0900

    added nltk punkt download-1

--- src/processor.py
+++ src/processor.py
@@ -1,4 +1,3 @@
-from msilib.schema import Error
 from typing import List
 
 from nltk import sent_tokenize
@@ -7,8 +6,7 @@ from nltk import sent_tokenize
 def process(text: str) -> List[str]:
     try:
         return sent_tokenize(text)
-    except Error as e:
-
+    except:
         import nltk
         nltk.download('punkt')
         return sent_tokenize(text)
